1c58537e41f6ea195b23a337be6e06123b1bc1f3
ngoctram21 <48621237+ngoctram21@users.noreply.github.com>
2020-01-03 19:11:16 +0100

    Add files via upload

---
+++ vba_project.cpp
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <fstream>
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <ctime>
+#include <complex>
+#include <vba_project.hpp>
+ 
+ using namespace std;
+
+  vector<vector<int> > Identity(int n)
+    { 
+       vector<vector<int> > matrix;
+       matrix = vector<vector<int> >(n, vector<int>(n,0));
+
+        // Set the diagonal to be 1
+        for(unsigned int t = 0; t < n; t++)
+            matrix[t][t] = 1;
+        return matrix;
+    } 
+
+  
+
+ int vba::Pricing_European_Options(double S0,double Maturity, double Strike, double r, double divid,double sigma, double* ptprice, double* ptrdelta, double* ptrgamma, double* ptrtheta, double* ptrrho, double* ptrvega, int nTime, int nSpace)
+ {
+   double h = Maturity/nTime,
+          b = r - divid -SQR(sigma)/2.0;
+   double l = abs(b)*Maturity + 3.89*sigma*sqrt(Maturity),
+          k = 2*l/(nSpace),
+          alpha = SQR(sigma)/(2.0*SQR(k)) - b/(2*k);
+   double beta = -(SQR(sigma)/SQR(k)+r);
+   double gamma = SQR(sigma)/(2*SQR(k)) + b/(2*k);
+
+        std::vector<double> x(nSpace+1), Payoff(nSpace+1), Price(nSpace+1);
+        for (int i = 0; i < nSpace; i++)
+        {
+            x[i] = log(S0) - l + i*k;
+            Payoff[i] = MAX(exp(x[i])-Strike, 0);
+            Price[i] = Payoff[i];
+        }
+        
+  }
+ 
---
+++ vba_project.hpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+#define SQR(x) (x*x);
+#define MAX(A,B) ( (A) > (B) ? (A):(B) )
+
+vector<vector<int> > Identity(int n){return Identity(n);}
+namespace vba
+{ 
+    int Pricing_European_Options(double S0, double Maturity, double Strike, double r, double divid,double sigma, double* ptprice, 
+                                double* ptrdelta, double* ptrgamma, double* ptrtheta, double* ptrrho, 
+                                double* ptrvega, int nTime, int nSpace){return vba::Pricing_European_Options(S0,
+                                Maturity,Strike, r, divid,sigma,ptprice,ptrdelta,ptrgamma,ptrtheta,ptrrho, 
+                                ptrvega, nTime,nSpace);}
+}
